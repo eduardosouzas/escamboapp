2606d651f05b17e748496a6de4ad78f64ecab1a6
Edu Souza <eduardomail@gmail.com>
2017-02-07 00:59:59 +0000

    aula de POROs

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -21,8 +21,8 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
   def create
-    @category = Category.new(params_category)
-    if @category.save
+    @category = CategoryService.create(params_category)
+    unless @category.errors.any?
       redirect_to backoffice_categories_path, notice: "Categoria (#{@category.description})  salva com sucesso!!!"
     else
       render :new
