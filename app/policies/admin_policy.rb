8f756c2f20ca104a1a2052d3e089b2a5f9d295fe
edu souza <eduardomail@gmail.com>
2017-02-08 19:15:06 +0000

    select option radio

--- app/policies/admin_policy.rb
+++ app/policies/admin_policy.rb
@@ -2,13 +2,16 @@ class AdminPolicy < ApplicationPolicy
 
   def new?
     user.full_access?
-
   end
 
 
   class Scope < Scope
     def resolve
-      scope
+      if user.full_access?
+         scope.all
+      else
+         scope.with_restricted_access
+      end
     end
   end
 end
