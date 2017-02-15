27d0a8537eca994af8df4bd5db45f82675c739b2
Edu souza <eduardomail@gmail.com>
2017-02-15 19:24:05 +0000

    subject email

--- app/controllers/backoffice/send_mail_controller.rb
+++ app/controllers/backoffice/send_mail_controller.rb
@@ -1,10 +1,15 @@
 class Backoffice::SendMailController < BackofficeController
 
-def edit
-    @admin = Admin.find(params[:id])
-end
+    def edit
+        @admin = Admin.find(params[:id])
+    end
 
-def create
+    def create
+      respond_to do |format|
+        format.js
 
-end
+      end
+
+
+    end
 end
